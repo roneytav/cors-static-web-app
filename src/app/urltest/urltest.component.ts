022a1237e20e99eaaf7ccca1a1bee54be2e6ae74
Roney <roneytavares@brq.com>
2023-07-18 19:01:28 -0300

    ajustado para mensagem do proprio erro

--- src/app/urltest/urltest.component.ts
+++ src/app/urltest/urltest.component.ts
@@ -8,20 +8,41 @@ import { HttpClient } from  '@angular/common/http';
 })
 export class UrltestComponent implements OnInit{
 http:HttpClient;
+statusUrl:string;
   constructor(private httpclient:HttpClient){
     this.http=httpclient;
   }
     ngOnInit(){
-
-      this.http.get('https://proud-wave-0410e5a10.3.azurestaticapps.net/').subscribe(
-        (res)=>{
-          console.log(res)
-        }
-        ,(error)=>{console.log(error)}
-      )
+      //'https://proud-wave-0410e5a10.3.azurestaticapps.net/
+      //https://storageteste1.z15.web.core.windows.net/
+      //'https://azrstufirsngmfincldevn.z15.web.core.windows.net/
+fetch('https://storageteste1.z15.web.core.windows.net/')
+.then(
   
+     
+        (res)=>{
+          if(res.status!=200){
+            this.statusUrl='Url offline'
+            throw res.statusText ;
+          console.log(res);
+          }else{
+            this.statusUrl='Online'
+                        console.log("url online")
+          }
+        }
+      )
+  .catch(
+    (error)=>
+  {
+  alert('ERRO ao conectar url contacte suporte descricao:'+error);
 
-    }
+  }
+  )
+
+  
+}
+
+    
 
   
 }
